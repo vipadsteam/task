c9b5b61cb6b98f5de5fe55a40e4140ce1c57ce45
xiaorui.ni <xiaorui.ni@vipshop.com>
2018-04-11 15:30:36 +0800

    add job common method

--- src/main/java/org/adam/goku/task/chain/income/Job.java
+++ src/main/java/org/adam/goku/task/chain/income/Job.java
@@ -3,10 +3,14 @@
  */
 package org.adam.goku.task.chain.income;
 
+import java.util.List;
+
 /**
  * @author USER
  *
  */
 public abstract class Job {
+
+	public abstract List<Task> getTasks();
 
 }
