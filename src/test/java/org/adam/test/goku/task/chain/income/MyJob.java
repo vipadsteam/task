599c03e35ee05883143cd6fd80eb335d12356f38
xiaorui.ni <xiaorui.ni@vipshop.com>
2018-04-11 21:09:11 +0800

    Job test update

--- src/test/java/org/adam/test/goku/task/chain/income/MyJob.java
+++ src/test/java/org/adam/test/goku/task/chain/income/MyJob.java
@@ -1,7 +1,15 @@
 package org.adam.test.goku.task.chain.income;
 
+import java.util.List;
+
 import org.adam.goku.task.chain.income.Job;
+import org.adam.goku.task.chain.income.Task;
 
 public class MyJob extends Job {
+
+	@Override
+	public List<Task> getTasks() {
+		return null;
+	}
 
 }
